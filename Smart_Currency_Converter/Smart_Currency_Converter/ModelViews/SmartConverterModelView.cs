b19169aeedbc696178604abd0a5907b9704aea35
Alireza <A.Sattari@outlook.com>
2020-01-28 22:36:04 -0800

    Initial setup

---
+++ Smart_Currency_Converter/Smart_Currency_Converter/ModelViews/SmartConverterModelView.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smart_Currency_Converter.ModelViews
+{
+    class SmartConverterModelView
+    {
+    }
+}
---
+++ Smart_Currency_Converter/Smart_Currency_Converter/SmartConverterPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Smart_Currency_Converter
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SmartConventerPage : ContentPage
+    {
+        public SmartConventerPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
