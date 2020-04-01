a8cf80d07cb8e75a92fe666999407690ba878f95
Alireza <A.Sattari@outlook.com>
2020-03-31 22:25:01 -0700

    The Function can extract text and digits from a photo | Credentials moved to Azure KeyVault

--- Smart_Currency_Converter/Smart_Currency_Converter_Cloud/AnalyzeImage.cs
+++ Smart_Currency_Converter/Smart_Currency_Converter_Cloud/AnalyzeImage.cs
@@ -1,55 +1,93 @@
-using System.IO;
+// https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/quickstarts/csharp-hand-text?tabs=version-2
+using System;
+using System.Linq;
+using Azure.Identity;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
-using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
 
 namespace Smart_Currency_Converter_Cloud
 {
     public static class AnalyzeImage
     {
-        private const string subscriptionKey = "2416c55d1d9342a5997a7c751cfc8b2f";
-        private const string endpoint = "https://scc-computervision.cognitiveservices.azure.com/";
+        private const string SubscriptionSecretName = "Computer-Vision-SubscriptionKey";
+        private const string EndpointSecretName = "Computer-Vision-Endpoint";
+
+        private static readonly string subscriptionKey = GetAzureKeyVaultValue(SubscriptionSecretName);
+        private static readonly string endpoint = GetAzureKeyVaultValue(EndpointSecretName);
+        private static readonly string uri = endpoint + "vision/v2.1/read/core/asyncBatchAnalyze";
 
         [FunctionName("AnalyzeImage")]
         public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestMessage req, ILogger log)
         {
-            log.LogInformation($"{nameof(AnalyzeImage)} Function is triggered ...");
+            log.LogInformation($"{nameof(AnalyzeImage)} Function is triggered ...");            
             string responseMessage = null;
 
             MultipartMemoryStreamProvider inputData = await req.Content.ReadAsMultipartAsync();
-            Stream imageAsStream = await inputData.Contents[0].ReadAsStreamAsync();
+            byte[] imageAsByteArray = await inputData.Contents[0].ReadAsByteArrayAsync();
+            // Stream imageAsStream = await inputData.Contents[0].ReadAsStreamAsync();
 
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
 
-            List<VisualFeatureTypes> features = new List<VisualFeatureTypes>()
+            HttpResponseMessage httpResponse;
+            string resultUri = null;   // Stores the URI of the second REST API method, returned by the first REST API method.
+
+            // Adds the image array as the request body
+            using (ByteArrayContent content = new ByteArrayContent(imageAsByteArray))
             {
-                VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
-                VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
-                VisualFeatureTypes.Tags, VisualFeatureTypes.Adult,
-                VisualFeatureTypes.Color, VisualFeatureTypes.Brands,
-                VisualFeatureTypes.Objects
-            };
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
 
-            ComputerVisionClient visionClient = Authenticate(endpoint, subscriptionKey);
-            ImageAnalysis results = await visionClient.AnalyzeImageInStreamAsync(imageAsStream, features);
-
-            foreach (var caption in results.Description.Captions) {
-                responseMessage = $"{caption.Text} with confidence {caption.Confidence}";
+                // First REST API method, Batch Read, starts the async process to analyze the written text in the image
+                httpResponse = await client.PostAsync(uri, content);
             }
+
+            if (httpResponse.IsSuccessStatusCode) {
+            
+                resultUri = httpResponse.Headers.GetValues("Operation-Location").FirstOrDefault();
+            
+            } else {
+                string errorString = await httpResponse.Content.ReadAsStringAsync();
+                // TODO: Log error message and tell the user something
+            }
+
+            int i = 0;
+            // If the first REST API method completes successfully, the second REST API method retrieves the text written in the image.
+            do {
+                System.Threading.Thread.Sleep(1000);
+
+                httpResponse = await client.GetAsync(resultUri);
+                responseMessage = await httpResponse.Content.ReadAsStringAsync();
+                
+                ++i;
+            
+            } while (i < 10 && responseMessage.IndexOf("\"status\":\"Succeeded\"") == -1);
+
 
             return new OkObjectResult(responseMessage);
         }
 
 
-        private static ComputerVisionClient Authenticate(string endpoint, string key)
+        private static string GetAzureKeyVaultValue(string secretName)
         {
-            ComputerVisionClient client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(key)) { Endpoint = endpoint };
-            return client;
+            string secretValue = null;
+
+            try {
+                SecretClient client = new SecretClient(vaultUri: new Uri("https://msft-ai-credentials.vault.azure.net/"), credential: new DefaultAzureCredential());
+
+                KeyVaultSecret secretClient = client.GetSecret(secretName).Value;
+                secretValue = secretClient.Value;
+
+            } catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+
+            return secretValue;
         }
     }
 }
