c2a6efbc76826387b436e675944b3ff4a5360cc1
Alireza <A.Sattari@outlook.com>
2020-05-22 16:35:33 -0700

    Structured the skeleton of Result page

--- Smart_Currency_Converter/Smart_Currency_Converter/ViewModels/ResultPageViewModel.cs
+++ Smart_Currency_Converter/Smart_Currency_Converter/ViewModels/ResultPageViewModel.cs
@@ -6,6 +6,7 @@ namespace ViewModel.Result
 {
     public class ResultPageViewModel
     {
+        /*
         public Command Convert { get; }
 
         public ResultPageViewModel()
@@ -22,6 +23,79 @@ namespace ViewModel.Result
 
             Converter converter = new Converter();
             decimal r = await converter.Convert(25.45M, baseCurrency, targetCurrency);
+        }
+        */
+        public List<Product> Productsss { get; set; }
+
+        public ResultPageViewModel()
+        {
+            Productsss = new ProductService().GetProductList();
+        }
+    }
+
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public string RandomName { get; set; }
+
+    }
+
+    public class ProductService
+    {
+        public List<Product> GetProductList()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    ProductName = "One",
+                    RandomName = "1"
+                },
+                new Product()
+                {
+                    ProductName = "two",
+                    RandomName = "2"
+                },
+                new Product()
+                {
+                    ProductName = "three",
+                    RandomName = "3"
+                },
+                new Product()
+                {
+                    ProductName = "four",
+                    RandomName = "4"
+                },
+                new Product()
+                {
+                    ProductName = "five",
+                    RandomName = "5"
+                }
+                ,
+                new Product()
+                {
+                    ProductName = "five",
+                    RandomName = "6"
+                }
+                ,
+                new Product()
+                {
+                    ProductName = "five",
+                    RandomName = "7"
+                }
+                ,
+                new Product()
+                {
+                    ProductName = "five",
+                    RandomName = "8"
+                }
+                ,
+                new Product()
+                {
+                    ProductName = "five",
+                    RandomName = "9"
+                }
+            };
         }
     }
 }
