205ada4c8242f8b96b402f3a2a179014d977c1e2
Alireza <A.Sattari@outlook.com>
2020-06-01 23:37:31 -0700

    Added animations to the loading page

--- Smart_Currency_Converter/Smart_Currency_Converter/LoadingPage.xaml.cs
+++ Smart_Currency_Converter/Smart_Currency_Converter/LoadingPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Lottie.Forms;
+﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,10 +7,21 @@ namespace Smart_Currency_Converter
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoadingPage : ContentPage
     {
+        private const string ANALYZING_ANIMATION = "animation-analyze.json";
+        private const string PROCESSSING_ANIMATION = "animation-process.json";
+
         public LoadingPage()
         {
             NavigationPage.SetHasNavigationBar(this, false);
             InitializeComponent();
+            LoadingAnimationView.Animation = PROCESSSING_ANIMATION;
+            Device.StartTimer(TimeSpan.FromSeconds(3), ChangeAnimation);
+        }
+
+        private bool ChangeAnimation()
+        {
+            LoadingAnimationView.Animation = ANALYZING_ANIMATION;
+            return false;
         }
     }
 }
