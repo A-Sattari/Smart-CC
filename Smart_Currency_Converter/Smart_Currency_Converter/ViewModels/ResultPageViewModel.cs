87e071f31172a3f74c15c9eda79b292aaa34e192
Alireza <A.Sattari@outlook.com>
2020-06-10 19:51:27 -0700

    Removed the Save button in Result page

--- Smart_Currency_Converter/Smart_Currency_Converter/ViewModels/ResultPageViewModel.cs
+++ Smart_Currency_Converter/Smart_Currency_Converter/ViewModels/ResultPageViewModel.cs
@@ -2,28 +2,24 @@
 using Xamarin.Forms;
 using System.Reflection;
 using Xamarin.Forms.Xaml;
-using System.ComponentModel;
 using Smart_Currency_Converter;
 using System.Collections.Generic;
 
 namespace ViewModel.Result
 {
-    public class ResultPageViewModel : INotifyPropertyChanged
+    public class ResultPageViewModel
     {
-        public event PropertyChangedEventHandler PropertyChanged;
         public static INavigation ModalNavigation;
         public static ImageSource Image;
         public static List<KeyValuePair<string, decimal>> ItemPricePairs { get; set; }
 
         public Command ShowTakenPhoto { get; }
         public Command RetakePhoto { get; }
-        public Command SavePhoto { get; }
 
         public ResultPageViewModel()
         {
             ShowTakenPhoto = new Command(DisplayTakenPhoto);
             RetakePhoto = new Command(OpenSmartConverterPage);
-            SavePhoto = new Command(SaveTakenPhoto);
         }
 
         private async void DisplayTakenPhoto() =>
@@ -31,10 +27,6 @@ namespace ViewModel.Result
 
         private async void OpenSmartConverterPage() =>
             await App.NavigationObj.PushAsync(new SmartConverterPage());
-
-        private void SaveTakenPhoto()
-        {
-        }
     }
 
 
