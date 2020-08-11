eaa91bbad6cdc8198226e50e75b9f297932495ff
Alireza <A.Sattari@outlook.com>
2020-08-10 19:45:47 -0700

    Minor code addition to DisconnectedInternet panel

--- Smart_Currency_Converter/Smart_Currency_Converter/InformativeViews/DisconnectedInternetView.xaml.cs
+++ Smart_Currency_Converter/Smart_Currency_Converter/InformativeViews/DisconnectedInternetView.xaml.cs
@@ -6,10 +6,18 @@ namespace Smart_Currency_Converter.InformativeViews
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DisconnectedInternetView : StackLayout
     {
+        private static StackLayout noficationScatckLayout;
+
         public DisconnectedInternetView()
         {
-            NavigationPage.SetHasNavigationBar(this, false);
+            //NavigationPage.SetHasNavigationBar(this, false);
             InitializeComponent();
+            noficationScatckLayout = NotificationPanel;
+        }
+
+        public static void SetVisibility(bool isVisible)
+        {
+            noficationScatckLayout.IsVisible = isVisible;
         }
     }
 }
